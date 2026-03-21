df19d2bf5f0c7ef866d440de1b7329a165ab6c28
DanyDiab <danydiab04@gmail.com>
2026-03-21 18:35:30 -0300

    constructing base set

--- scripts/tf-idf.py
+++ scripts/tf-idf.py
@@ -1,5 +1,5 @@
 from re import search
-from db import Database, DOC_LENGTH_TABLE, INVERTED_INDEX_TABLE, DOCUMENTS_TABLE
+from db import Database, DOC_LENGTH_TABLE, INVERTED_INDEX_TABLE, DOCUMENTS_TABLE, LINKS_TABLE
 import nltk
 from nltk.corpus import words, stopwords
 
@@ -9,57 +9,62 @@ english_dict = set(words.words())
 stop_words = set(stopwords.words())
 
 pageLengthFilter = 500
+numToReturn = 200
 
 def isWordValid(word):
     if english_dict is None:
         raise RuntimeError("nltk is not installed")
     return word.lower() in english_dict
 
-def calculateTermFrequency(db: Database):
+def calculateTF_IDF(db: Database):
     search_query = "Mountains of the world"
 
     normalized_query = search_query.lower()
     filtered_query = [word for word in normalized_query.split() if word not in stop_words]
     in_query = ",".join(f"'{t}'" for t in filtered_query)
     query = f"""
-        SELECT
-            dm.doc_id,
-            dm.page_name,
-            SUM((i.word_count * 1.0 / dl.page_length) * idf.idf) AS tf_idf
-        FROM {INVERTED_INDEX_TABLE} i
-        JOIN TERMS t ON t.term_id = i.term_id
-        JOIN {DOC_LENGTH_TABLE} dl ON dl.doc_id = i.doc_id
-        JOIN {DOCUMENTS_TABLE} dm ON dm.doc_id = i.doc_id
-
-        JOIN (
+        WITH TF_IDF AS(
             SELECT
-                t2.term,
-                LOG(
-                    (SELECT COUNT(*) FROM {DOC_LENGTH_TABLE}) * 1.0 /
-                    COUNT(DISTINCT i2.doc_id)
-                ) AS idf
-            FROM {INVERTED_INDEX_TABLE} i2
-            JOIN TERMS t2 ON t2.term_id = i2.term_id
-            WHERE t2.term IN ({in_query})
-            GROUP BY t2.term
-        ) AS idf ON idf.term = t.term
+                dm.doc_id
+            FROM {INVERTED_INDEX_TABLE} i
+            JOIN TERMS t ON t.term_id = i.term_id
+            JOIN {DOC_LENGTH_TABLE} dl ON dl.doc_id = i.doc_id
+            JOIN {DOCUMENTS_TABLE} dm ON dm.doc_id = i.doc_id
 
-        WHERE t.term IN ({in_query}) AND dl.page_length > {pageLengthFilter}
+            JOIN (
+                SELECT
+                    t2.term,
+                    LOG(
+                        (SELECT COUNT(*) FROM {DOC_LENGTH_TABLE}) * 1.0 /
+                        COUNT(DISTINCT i2.doc_id)
+                    ) AS idf
+                FROM {INVERTED_INDEX_TABLE} i2
+                JOIN TERMS t2 ON t2.term_id = i2.term_id
+                WHERE t2.term IN ({in_query}) 
+                GROUP BY t2.term
+            ) AS idf ON idf.term = t.term
 
-        GROUP BY dm.doc_id
-        ORDER BY tf_idf DESC
-        LIMIT 20
+            WHERE t.term IN ({in_query}) AND dl.page_length > {pageLengthFilter}
+
+            GROUP BY dm.doc_id
+            ORDER BY SUM((i.word_count * 1.0 / dl.page_length) * idf.idf) DESC
+            LIMIT {numToReturn}
+        )
+        SELECT DISTINCT i.doc_id FROM {LINKS_TABLE} i
+        JOIN {DOC_LENGTH_TABLE} dl ON dl.doc_id = i.doc_id
+        WHERE (i.doc_id IN TF_IDF OR i.link_id IN TF_IDF) AND dl.page_length > {pageLengthFilter}
     """
 
     res = db.executeQuery(query)
     for val in res:
         print(val)
+    print(len(res))
 
 
 
 def main():
     database = Database()
-    calculateTermFrequency(db=database)
+    calculateTF_IDF(db=database)
 
 if __name__ == "__main__":
     main()
